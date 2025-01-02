673146bdd798f5a8854548f961f4c4a48b4df117
domaxduk <domaxduk@gmail.com>
2025-01-02 15:31:52 +0700

    Update AVPlayerExtension.swift

--- Sources/SakuraExtension/AVPlayerExtension.swift
+++ Sources/SakuraExtension/AVPlayerExtension.swift
@@ -15,21 +15,21 @@ public extension AVPlayer {
     
     func seek(percent: CGFloat) {
         let second = CGFloat(percent * self.duration.seconds)
-        let time = CMTime(seconds: second, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        let time = CMTime(seconds: second, preferredTimescale: 10000)
         self.seek(to: time)
     }
     
     func rewindVideo(by seconds: Double) {
         let currentTime = self.currentTime()
         let newTime = max(currentTime.seconds - seconds, 0.0)
-        let time = CMTime(seconds: newTime, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        let time = CMTime(seconds: newTime, preferredTimescale: 10000)
         self.seek(to: time)
     }
     
     func forwardVideo(by seconds: Double) {
         let currentTime = self.currentTime()
         let newTime = currentTime.seconds + seconds
-        let time = CMTime(seconds: newTime, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        let time = CMTime(seconds: newTime, preferredTimescale: 10000)
         self.seek(to: time)
     }
 }
