31e608e64a835df2db3a9b7cb63b05411085683b
domaxduk <domaxduk@gmail.com>
2025-01-09 09:22:49 +0700

    Update BlurSwiftUIView.swift

--- Sources/SakuraExtension/SwiftUI/View/BlurSwiftUIView.swift
+++ Sources/SakuraExtension/SwiftUI/View/BlurSwiftUIView.swift
@@ -8,16 +8,16 @@
 import Foundation
 import SwiftUI
 
-struct BlurSwiftUIView: UIViewRepresentable {
+public struct BlurSwiftUIView: UIViewRepresentable {
     var effect: UIBlurEffect
     
-    func makeUIView(context: Context) -> UIVisualEffectView {
+    public func makeUIView(context: Context) -> UIVisualEffectView {
         let view = UIVisualEffectView(effect: effect)
         view.backgroundColor = .clear
         return view
     }
     
-    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+    public func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
         
     }
 }
