21155db5b4fc84991fbb3376a2ff6331a80733fb
domaxduk <domaxduk@gmail.com>
2025-01-10 11:14:01 +0700

    update

--- Sources/SakuraExtension/ApplicationExtension.swift
+++ Sources/SakuraExtension/ApplicationExtension.swift
@@ -21,3 +21,33 @@ public extension UIApplication {
         }
     }
 }
+
+public extension UIApplication {
+    var keyWindow: UIWindow? {
+        return self.connectedScenes
+               .filter { $0.activationState == .foregroundActive } // Keep only active scenes, onscreen and visible to the user
+               .first(where: { $0 is UIWindowScene })  // Keep only the first `UIWindowScene`
+               .flatMap({ $0 as? UIWindowScene })?.windows // Get its associated windows
+               .first(where: \.isKeyWindow) // Finally, keep only the key window
+    }
+    
+    var rootViewController: UIViewController? {
+        return keyWindow?.rootViewController
+    }
+    
+    var navigationController: UINavigationController? {
+        return rootViewController as? UINavigationController
+    }
+}
+
+public extension UINavigationController {
+    var topVC: UIViewController? {
+        var topVC = self.topViewController
+        
+        while topVC?.presentedViewController != nil {
+            topVC = topVC?.presentedViewController
+        }
+        
+        return topVC
+    }
+}
